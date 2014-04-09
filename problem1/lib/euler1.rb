eb87e08f07cefe0c39ac46da7c0cc385ceebe087
Robert Leon <llexi@hotmail.com>
2014-04-09 15:10:21 +0100

    First RSPEC test passed, multiples of 3

---
+++ problem1/lib/euler1.rb
@@ -0,0 +1,3 @@
+def divisible_by3(number)
+	number % 3 == 0
+end
---
+++ problem1/spec/euler1_spec.rb
@@ -0,0 +1,23 @@
+require 'euler1'
+
+describe 'euler1' do
+
+it "knows that 9 is a multiple of 3" do
+	expect(divisible_by3(9)).to be_true
+end
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+end
+
