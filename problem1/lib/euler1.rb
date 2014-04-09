24a838b4ebbb4f099de7cfb23ce84248d7523819
Robert Leon <llexi@hotmail.com>
2014-04-09 15:19:10 +0100

    Tested that methods understand multiples of 3 and 5

--- problem1/lib/euler1.rb
+++ problem1/lib/euler1.rb
@@ -1,3 +1,7 @@
 def divisible_by3(number)
 	number % 3 == 0
 end
+
+def divisible_by5(number)
+	number % 5 == 0
+end
--- problem1/spec/euler1_spec.rb
+++ problem1/spec/euler1_spec.rb
@@ -6,6 +6,18 @@ it "knows that 9 is a multiple of 3" do
 	expect(divisible_by3(9)).to be_true
 end
 
+it "knows that 8 is not a multiple of 3" do
+	expect(divisible_by3(8)).to be_false
+end
+
+it "knows that 10 is a multiple of 5" do
+	expect(divisible_by5(10)).to be_true
+end
+
+it "knows that 7 if not multiple of 5" do
+	expect(divisible_by5(7)).to be_false
+end
+
 
 	
 
